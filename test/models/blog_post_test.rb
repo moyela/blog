4854dfdc7fad709df70fab2b0ce1bc74eb09a6ca
Wemi Moyela <wemimoyela@gmail.com>
2025-03-26 11:51:54 +0100

    changed index title and added blog count

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is_draft? returns true for draft post" do
+    assert BlogPost.new(published_at: nil).is_draft?
+  end
 end
